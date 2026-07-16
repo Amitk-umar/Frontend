d07376159756b9b01bfdd02ef83ce7179e0f269f
amit <amit6871806@gmail.com>
2026-07-16 18:22:57 +0530

    css border animation cards

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+console.log("hello , JavaScript");
+console.warn('This is warning!')
+console.error('This is error')
+console.table(['Ankit Jangir','Ravi sharma','Raghav Sharma'])
+
+// ------------------Variables--------------------
+var a = 10
+var b = 20
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
