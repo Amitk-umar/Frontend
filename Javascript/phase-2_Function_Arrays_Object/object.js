76c0efc951f12af48cd5d834d58aba97b6eca5ed
amit <amit6871806@gmail.com>
2026-08-12 00:01:08 +0530

    object and methods

--- Javascript/phase-2_Function_Arrays_Object/object.js
+++ Javascript/phase-2_Function_Arrays_Object/object.js
@@ -2,12 +2,12 @@
 //An object in JavaScript is a standalone entity that stores data as a collection of properties (key-value pairs). Properties can hold any data type, including strings, numbers, arrays, or functions (which are called methods).
 
 //Syntax
-    // const user = {
-    //   name: "Alex",
-    //   age: 25,
-    //   city: 'Delhi'
-    // };
-    // console.log(user);
+// const user = {
+//   name: "Alex",
+//   age: 25,
+//   city: 'Delhi'
+// };
+// console.log(user);
 
 
 //CRUD Operations on JavaScript Objects
@@ -62,30 +62,66 @@
 
 
 
-// 1. Get Only the Keys (Object.keys())
-const keys = Object.keys(smartphone);
+// // 1. Get Only the Keys (Object.keys())
+// const keys = Object.keys(smartphone);
 
-console.log(keys);
-// Output: ['brand', 'model', 'price', 'storage']
+// console.log(keys);
+// // Output: ['brand', 'model', 'price', 'storage']
 
-// Example use case: Finding the total number of properties
-console.log(`The object has ${keys.length} specifications.`); 
-// Output: The object has 4 specifications.
-
-
+// // Example use case: Finding the total number of properties
+// console.log(`The object has ${keys.length} specifications.`); 
+// // Output: The object has 4 specifications.
 
 
-// 2. Get Only the Values (Object.values())
-const values = Object.values(smartphone);
 
-console.log(values);
-// Output: ['Samsung', 'Galaxy S26', 999, '256GB']
 
-// Example use case: Checking if a specific value exists
-const hasHighStorage = values.includes("256GB");
-console.log(hasHighStorage); 
-// Output: true
+// // 2. Get Only the Values (Object.values())
+// const values = Object.values(smartphone);
 
+// console.log(values);
+// // Output: ['Samsung', 'Galaxy S26', 999, '256GB']
+
+// // Example use case: Checking if a specific value exists
+// const hasHighStorage = values.includes("256GB");
+// console.log(hasHighStorage); 
+// // Output: true
+
+
+
+// Object.entries() -  The Object.entries() MDN Web Docs method takes an object and converts it into a two-dimensional array of [key, value] pairs. It is heavily used for looping over objects with array methods.
+
+// const friendStatus =  {
+//    Name:'Ankit Jangir',
+//     Age : 23,
+//     city : "Ayodhya",
+//     isMarried  : true
+// }
+// console.log(friendStatus); //{Friend Name: 'Ankit Jangir', Age: 23, city: 'Ayodhya', isMarried: true}
+// console.log(Object.entries(friendStatus)); //(4) [Array(2), Array(2), Array(2), Array(2)]
+
+// // Common use case: Looping with destructuring
+// for(const [key,value] of Object.entries(friendStatus)){
+//     console.log(`Key : ${key} And Value : ${value}`);
+// }
+
+
+
+
+const company = {
+    name: "TechCorp",
+    // Nested Object
+    location: {
+        city: "Switzerland",
+        country: "Europe"
+    },
+    // Nested Array containing objects
+    employees: [
+        { id: 1, name: "Bhola", role: "Developer" },
+        { id: 2, name: "jackson", role: "Designer" }
+    ]
+};
+console.log(company.location.city); //Switzerland
+console.log(company.employees[0].name); //Bhola
 
 
 
