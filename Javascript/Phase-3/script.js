ce2fbdd15683d2c08e513f238c81065b1262cd80
amit <amit6871806@gmail.com>
2026-08-19 22:39:01 +0530

    Js Errors and lexical scope and closure example in script.js file

--- Javascript/Phase-3/script.js
+++ Javascript/Phase-3/script.js
@@ -10,4 +10,27 @@
 
 
 
+const globalVar = "I am global";
+
+function outerFunction() {
+  const outerVar = "I am outside";
+
+  function innerFunction() {
+    const innerVar = "I am inside";
+    
+    // innerFunction can see its own scope, its parent's scope, and the global scope
+    console.log(innerVar);  // Works
+    console.log(outerVar);  // Works
+    console.log(globalVar); // Works
+  }
+
+  innerFunction();
+  
+}
+// outerFunction CANNOT see inside innerFunction
+// console.log(innerVar); -> ReferenceError: innerVar is not defined
+// console.log(outerVar); -> ReferenceError: outerVar is not defined
+
+
+outerFunction();
 
