edd6efb8dc2d0aa6f76b09d100f7c0070150954a
amit <amit6871806@gmail.com>
2026-09-05 00:07:04 +0530

    input.value VS input.getAttribute in js dom

--- DOM/day3/script.js
+++ DOM/day3/script.js
@@ -6,23 +6,41 @@
 
 // example  : width = 200px ;       [width = Attribute and Property = 200px ]
 
-const p= document.querySelector("#uid")
+// const p= document.querySelector("#uid")
 
-const setClass = p.setAttribute("class","c1 c2 c3")
+// const setClass = p.setAttribute("class","c1 c2 c3")
 
-const removeClass = p.removeAttribute("class")
+// const removeClass = p.removeAttribute("class")
 
-const getClass = p.getAttribute("class")
+// const getClass = p.getAttribute("class")
 
-const hasClass = p.hasAttribute("class") 
-console.log(hasClass); //false
+// const hasClass = p.hasAttribute("class") 
+// console.log(hasClass); //false
 
-console.log(getClass); //null
+// console.log(getClass); //null
 
 
 
-//valid custom attribute with data- and update with dataset using method with camelCase
-const userCard = document.querySelector("#user-card")
-console.log(userCard.getAttribute('data-user-id')); //123
-userCard.dataset.userId = "9300"
-console.log(userCard.getAttribute('data-user-id')); //9300
+// //valid custom attribute with data- and update with dataset using method with camelcase property
+// const userCard = document.querySelector("#user-card")
+// console.log(userCard.getAttribute('data-user-id')); //123
+// userCard.dataset.userId = "9300"
+// console.log(userCard.getAttribute('data-user-id')); //9300
+
+
+
+
+
+//input.value VS input.getAttribute
+
+const inp = document.querySelector("input")
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", () => {
+
+    console.log(inp.value); //this is live
+    console.log(inp.getAttribute("value"));  //but this static ,the value we give in input tag in html
+})
+
+
+
