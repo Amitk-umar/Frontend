81410f7dd2679626056c84d86b976ef60dcb8c9a
amit <amit6871806@gmail.com>
2026-09-09 00:08:34 +0530

    todolist with edit and delete functionality using dom

--- DOM/projects/todolist/script.js
+++ DOM/projects/todolist/script.js
@@ -4,7 +4,7 @@ const todoBox = document.querySelector('.todo-list')
 
 add.addEventListener('click', () => {
   const value = input.value;
-if(value.trim() === "") return;
+  if (value.trim() === "") return;
 
   todoBox.innerHTML += `  <div class="li">
           <h3>${value}</h3>
@@ -14,10 +14,30 @@ if(value.trim() === "") return;
           </div>
         </div>`
 
-input.value =""; 
+  input.value = "";
 
 })
 
+//delete
+todoBox.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('del')) return
+  else {
+    e.target.closest('.li').remove()
+  }
+})
 
-const edit = document.querySelector('.edit')
-console.log(edit);
+//edit
+let editItem = null
+
+todoBox.addEventListener('click', (e) => {
+
+  if (!e.target.classList.contains('edit')) return
+
+  editItem = e.target.closest('.li')
+
+  const h3 = editItem.querySelector('h3')
+
+  input.value = h3.textContent
+
+  editItem.remove()
+})
